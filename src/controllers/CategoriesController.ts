0717d0f9476ec231a214b2825d8f1ac232de2af6
Cristian Quintanilla <cristiancbtis130@gmail.com>
2022-06-11 17:26:25 -0500

    get category by id

--- src/controllers/CategoriesController.ts
+++ src/controllers/CategoriesController.ts
@@ -3,15 +3,24 @@ import { Request, Response } from 'express';
 import CategoryModel, { Category } from '../models/Category';
 
 class CategoriesController {
-  public async getCategories (_: Request, res: Response) {
+  public async getCategories(_: Request, res: Response) {
     console.log('get categories');
   }
 
-  public async getCategory (req: Request, res: Response) {
-    console.log('get category');
+  public async getCategory(req: Request, res: Response) {
+    const { id } = req.params;
+    CategoryModel.findById(id).exec().then(category => {
+      if (!category) {
+        return res.status(404).json({ ok: false, msg: 'Category not found' });
+      }
+
+      res.status(200).json({ ok: true, category });
+    }).catch(() => {
+      res.status(500).json({ ok: false, msg: 'An error ocurred while getting the category' });
+    });
   }
 
-  public async createCategory (req: Request, res: Response) {
+  public async createCategory(req: Request, res: Response) {
     try {
       const { name } = req.body;
 
@@ -22,18 +31,19 @@ class CategoriesController {
       await category.save();
       res.status(201).json({
         category,
-        message: 'Category created successfully',
+        ok: true,
+        msg: 'Category created successfully',
       });
     } catch (err) {
-      res.status(500).json({ msg: 'An error ocurred while creating the category.' });
+      res.status(500).json({ ok: false, msg: 'An error ocurred while creating the category' });
     }
   }
 
-  public async updateCategory (req: Request, res: Response) {
+  public async updateCategory(req: Request, res: Response) {
     console.log('update category');
   }
 
-  public async deleteCategory (req: Request, res: Response) {
+  public async deleteCategory(req: Request, res: Response) {
     console.log('delete category');
   }
 }
